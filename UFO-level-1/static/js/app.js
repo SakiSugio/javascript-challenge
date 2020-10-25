66ad0033aef15ae1b63f372887220f80b951cbfd
Saki Sugio <sakisugio9@gmail.com>
2020-10-24 19:07:23 -0500

    dataset appended

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
+// Use D3 to select the table
+var table = d3.select("table");
+
+
 // YOUR CODE HERE!
+function createDefault() {
+    data.forEach(datas => {
+        row = table.append("tr")
+        row.append("td").text(datas.datetime);
+        row.append("td").text(datas.city);
+        row.append("td").text(datas.state);
+        row.append("td").text(datas.country);
+        row.append("td").text(datas.shape);
+        row.append("td").text(datas.durationMinutes);
+        row.append("td").text(datas.comments);
+    });
+}
+createDefault();
+
