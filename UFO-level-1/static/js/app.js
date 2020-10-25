23dba790b5c0628f2bc40e751f29ec94ba9d1afb
Saki Sugio <sakisugio9@gmail.com>
2020-10-24 22:54:08 -0500

    prevent default

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,7 +24,11 @@ var dataSearchField = d3.select("#datetime");
 var dataSearchButton = d3.select("#filter-btn");
 var table = d3.select("#ufo-table");
 
-dataSearchButton.on("click", () => {
+dataSearchButton.on("click", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    
     var dataSearchFor = dataSearchField.property("value");
     console.log(dataSearchFor);
     tbody.remove();
@@ -35,15 +39,6 @@ dataSearchButton.on("click", () => {
         return date.datetime == dataSearchFor});
     console.log(filteredDate)
 
-    // filteredDate.forEach(date => {
-    //         row.append("td").text(date.datetime);
-    //         row.append("td").text(date.city);
-    //         row.append("td").text(date.state);
-    //         row.append("td").text(date.country);
-    //         row.append("td").text(date.shape);
-    //         row.append("td").text(date.durationMinutes);
-    //         row.append("td").text(date.comments);
-    // })
     filteredDate.forEach(datas => {
             var row = tbody.append("tr");
             row.append("td").text(datas.datetime);
@@ -55,4 +50,29 @@ dataSearchButton.on("click", () => {
             row.append("td").text(datas.comments);
         });
 
-})
+
+
+        // dataSearchButton.on("click", () => {
+        //     var dataSearchFor = dataSearchField.property("value");
+        //     console.log(dataSearchFor);
+        //     tbody.remove();
+        //     tbody = table.append("tbody");
+        
+        //     // Filter date using datetime
+        //     var filteredDate = tableData.filter(function(date){
+        //         return date.datetime == dataSearchFor});
+        //     console.log(filteredDate)
+        
+        //     filteredDate.forEach(datas => {
+        //             var row = tbody.append("tr");
+        //             row.append("td").text(datas.datetime);
+        //             row.append("td").text(datas.city);
+        //             row.append("td").text(datas.state);
+        //             row.append("td").text(datas.country);
+        //             row.append("td").text(datas.shape);
+        //             row.append("td").text(datas.durationMinutes);
+        //             row.append("td").text(datas.comments);
+        //         });
+// })
+
+}
