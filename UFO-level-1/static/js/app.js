9acc6fd7aa8e1ce307601a1a336a7f4d1945aa20
Saki Sugio <sakisugio9@gmail.com>
2020-10-24 21:21:48 -0500

    filterrrrrr

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,14 +1,13 @@
 // from data.js
 var tableData = data;
 
-// Use D3 to select the table
-var table = d3.select("table");
-
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
 // YOUR CODE HERE!
 function createDefault() {
     data.forEach(datas => {
-        row = table.append("tr")
+        row = tbody.append("tr")
         row.append("td").text(datas.datetime);
         row.append("td").text(datas.city);
         row.append("td").text(datas.state);
@@ -20,3 +19,30 @@ function createDefault() {
 }
 createDefault();
 
+
+var dataSearchField = d3.select("#datetime");
+var dataSearchButton = d3.select("#filter-btn");
+
+
+dataSearchButton.on("click", () => {
+    var dataSearchFor = dataSearchField.property("value");
+    console.log(dataSearchFor);
+    // tbody.remove();
+    // tbody = table.append("tbody");
+
+    var filteredDate = tableData.filter(function(date){
+        return date.datetime == dataSearchFor});
+    console.log(filteredDate)
+        // .forEach(datas => {
+        //     var row = tbody.append("tr");
+        //     row = tbody.append("tr")
+        //     row.append("td").text(datas.datetime);
+        //     row.append("td").text(datas.city);
+        //     row.append("td").text(datas.state);
+        //     row.append("td").text(datas.country);
+        //     row.append("td").text(datas.shape);
+        //     row.append("td").text(datas.durationMinutes);
+        //     row.append("td").text(datas.comments);
+        // });
+
+})
